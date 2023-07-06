f17f949ae0b246bdc6fe6861db2a13e25f6a4512
Luke Tao <lyt5@cornell.edu>
2023-07-05 21:56:45 -0400

    Complete exercises 8-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toLowerCase().replace(/\s+/g, '');
+  return isPalindrome(str);
 };
+
+const isPalindrome = function (str) {
+  if (str.length < 2) {
+    return true;
+  }
+  if (str.first() === str.last()) {
+    return isPalindrome(str.substring(1, str.length()));
+  }
+  else {
+    return false;
+  }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
